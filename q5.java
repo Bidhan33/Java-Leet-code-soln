c14bcad30b51051f0eb21b93fbb8723a62910a21
bidan <bidhan.adhikari@myy.haaga-helia.fi>
2025-01-12 18:16:05 +0200

    Soln of Java

--- q5.java
+++ q5.java
@@ -1,40 +1,12 @@
-
-// {Enter a decimal number: 2,5
-// Enter a decimal number: 3,1415926
-
-// 2,50 * 3,14 = 7,85
-// 2,50 * 3,14 = 7,853981
-
-
-
-
-import java.text.DecimalFormat;
 import java.util.Scanner;
-
-public class q5 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Enter a decimal number: ");
-        double number1 = readDouble(input);
-
-        System.out.print("Enter a decimal number: ");
-        double number2 = readDouble(input);
-
-        double result = number1 * number2;
-
-        DecimalFormat decimalFormatTwo = new DecimalFormat("0.00");
-        DecimalFormat decimalFormatSix = new DecimalFormat("0.000000");
-
-        System.out.println("\n" + decimalFormatTwo.format(number1) + " * " + decimalFormatTwo.format(number2) +
-                " = " + decimalFormatTwo.format(result));
-        System.out.println(decimalFormatTwo.format(number1) + " * " + decimalFormatTwo.format(number2) +
-                " = " + decimalFormatSix.format(result));
+public class q5{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print ("Enter the number : ");
+        double num1 = Double.parseDouble(scanner.nextLine());
+        System.out.print ("Enter the number : ");
+        double num2 = Double.parseDouble(scanner.nextLine());
+        
     }
 
-    private static double readDouble(Scanner scanner) {
-        String input = scanner.nextLine().replace(',', '.');
-        return Double.parseDouble(input);
-    }
 }
-
---
+++ v1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class v1 {
+  
+    public static void main(String[] args) {
+         
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the subject: ");
+        String Subject = scanner.nextLine();
+        System.out.print("Enter the verb: ");
+        String Verb = scanner.nextLine();
+        System.out.print("Enter the object: ");
+        String Object = scanner.nextLine();
+        scanner.close();
+        System.out.println( Subject + " " + Verb + " " + Object);
+
+
+        
+    }
+}
